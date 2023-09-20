d55991b40ee6384ffcd11666ec344712feb0c783
Jonathan <jonathan.ramos@ikasle.aeg.eus>
2023-09-20 11:56:20 +0200

    [NEW] Creado metodo createPotion y función isPotionOfSanity

--- Cauldron.mjs
+++ Cauldron.mjs
@@ -6,4 +6,34 @@ export default class Cauldron{
 
         this.ingredients = ingredients;
     }
+
+    createPotion(ingredientName1, ingredientName2){
+
+        const ingredient1 = this.ingredients.find(ingredientName1);
+        const ingredient2 = this.ingredients.find(ingredientName2);
+
+        const commonEffects = ingredient1.findCommonEffects(ingredient2);
+        
+        if(commonEffects.length === 0){
+
+            return Potion.failed();
+            
+        }else if(isPotionOfSanity(ingredient1, ingredient2)){
+
+            return Potion.sanity();
+        }else{
+            
+            return Potion.with(
+                commonEffects[0],
+                ingredient1.weight + ingredient2.weight,
+                ingredient1.value + ingredient2.value
+            );
+        }
+    }
+}
+
+function isPotionOfSanity(i1,i2){
+        
+    return i1.name === `Nightshade` && i2.name === `Ectoplasm` ||
+    i1.name === `Ectoplasm` && i2.name === `Nightshade` ? true : false;
 }
