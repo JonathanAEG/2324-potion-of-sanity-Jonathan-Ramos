301ddcc245c941c6f450d2eb148f3d0cf0c7a333
Jonathan <jonathan.ramos@ikasle.aeg.eus>
2023-09-20 12:03:14 +0200

    [ADD] Creados métodos extras en para Ingredients e Ingredient

--- Ingredient.mjs
+++ Ingredient.mjs
@@ -19,4 +19,17 @@ export default class Ingredient{
             weight
         )
     }
+
+    hasName(name){
+        return this.name === name;
+    }
+
+    findCommoEffects(otherIngredient){
+        return this.effects.filter(effect=> otherIngredient.hasEffect(effect));
+    }
+
+    hasEffect(effect){
+
+        return this.effects.some(candidate=> effect.name === candidate.name);
+    }
 }
